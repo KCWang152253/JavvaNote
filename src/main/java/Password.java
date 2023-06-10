dbba4948605dc40e8e877c6fce6eab9f0da6d3fa
2747962529@qq.com <2747962529@qq.com>
2023-06-11 02:12:42 +0800

    java笔记提交20230612

---
+++ src/main/java/Password.java
@@ -0,0 +1,36 @@
+import lombok.Data;
+
+/**
+ * @author KCWang
+ * @version 1.0  常用 相关用户及密码
+ * @date 2023/6/11 上午1:46
+ */
+
+public enum Password {
+
+    GIT_HUB("mysql root", "kcwang"),
+    ;
+    private String user;
+    private String password;
+
+    Password(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
