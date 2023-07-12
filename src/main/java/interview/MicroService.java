e7d348f2c4e46cfb6a12caca4a4b83e6b149ce83
2747962529@qq.com <2747962529@qq.com>
2023-07-12 18:19:57 +0800

    20230710

---
+++ src/main/java/interview/MicroService.java
@@ -0,0 +1,9 @@
+package interview;
+
+/**
+ * @author KCWang
+ * @version 1.0
+ * @date 2023/7/12 下午5:37
+ */
+public class MicroService {
+}
---
+++ src/main/java/interview/QuestionInProjiect.java
@@ -0,0 +1,14 @@
+package interview;
+
+/**
+ * @author KCWang
+ * @version 1.0
+ * @date 2023/7/12 下午6:01
+ *
+ * 项目上遇到的问题
+ * 1、重复入账 线程池的线程复用时ThreadLocal的值没有清除
+ * 2、性能优化大内存机制
+ * 3、批量中断重试
+ */
+public class QuestionInProjiect {
+}
