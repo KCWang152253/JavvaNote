74df19e12593a18f460c6f78dddf2528e715ecd2
2747962529@qq.com <2747962529@qq.com>
2023-07-17 15:01:10 +0800

    20230717 Redis

---
+++ src/main/java/interview/HashCodeAndEquals.java
@@ -0,0 +1,16 @@
+package interview;
+
+/**
+ * @author KCWang
+ * @version 1.0
+ * @date 2023/7/17 下午2:51
+ *
+ *   ==和equals的区别
+ *   ==：比较的是引用
+ *   equals：在没有重写的时候比较的也是引用，重写后比的是值
+ *   hashcode的好处:
+ *              1、提高效率（毕竟hash过相等的元素不会太多）
+ *              2、保证准确（equals）
+ */
+public class HashCodeAndEquals {
+}
