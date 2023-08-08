4327f2b74d1a3375b66742826af96f4a8ef43241
2747962529@qq.com <2747962529@qq.com>
2023-08-08 11:26:43 +0800

    上海银行

---
+++ src/main/java/shangyin/ShangYin.java
@@ -0,0 +1,14 @@
+package shangyin;
+
+/**
+ * @author KCWang
+ * @version 1.0 上海银行
+ * @date 2023/8/8 上午9:26
+ *
+ *
+ *  上银开发e云桌面：kf7359  B@s95594!
+ *              e连：CS7018   Wkckf7359!
+ */
+public class ShangYin {
+
+}
