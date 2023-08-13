66f92f3ea5242ef3d3b17eaa7466f08eaa7bde11
2747962529@qq.com <2747962529@qq.com>
2023-08-13 14:47:14 +0800

    mq和上海银行

---
+++ src/main/java/shangyin/Document.java
@@ -0,0 +1,14 @@
+package shangyin;
+
+/**
+ * @author KCWang
+ * @version 1.0
+ * @date 2023/8/8 下午3:04
+ *
+ *      C语言 分期函数后缀 txn
+ *      分期开发文档
+ *          1.授信、用信
+ *
+ */
+public class Document {
+}
