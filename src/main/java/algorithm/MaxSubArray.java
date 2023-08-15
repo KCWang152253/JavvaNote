f5d78a6344f08c069848720373094049f33c3e3b
2747962529@qq.com <2747962529@qq.com>
2023-08-15 21:21:35 +0800

    算法

--- src/main/java/algorithm/MaxSubArray.java
+++ src/main/java/algorithm/MaxSubArray.java
@@ -109,4 +109,29 @@ public class MaxSubArray {
 
     }
 
+
+    /***
+     * @Description 股票最大收益
+     * @param
+     * @Return int
+     * @Author K.C.Wang
+     * @Date 2023/8/15 下午9:16
+     **/
+    private int maxStockInterest(){
+        int[] a = {7,6,5,4,3};
+        int max = a[0];
+        int min = a[0];
+        boolean flag = false;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i]>=max ){
+                max = a[i];
+                flag = true;
+            }else if(a[i]<=min && flag) {
+                min = a[i];
+                continue;
+            }
+        }
+        return max-min;
+    }
+
 }
