2ca469f98ddd8e7c2aceb479089b870ef587915a
2747962529@qq.com <2747962529@qq.com>
2023-08-19 09:53:04 +0800

    密码

--- src/main/java/shangyin/ShangYin.java
+++ src/main/java/shangyin/ShangYin.java
@@ -9,6 +9,7 @@ package shangyin;
  *  上银开发e云桌面：kf7359  B@s95594!
  *              e连：CS7018   Wkckf7359!
  *              调度平台：admin 123456
+ *              邮箱：wangkch@dev.bosc   B@s95594!1
  */
 public class ShangYin {
 
