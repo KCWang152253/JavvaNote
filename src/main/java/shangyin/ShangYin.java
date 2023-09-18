804076fbbfc6d2b40f48bf0958312c09bc81546b
2747962529@qq.com <2747962529@qq.com>
2023-09-18 12:16:24 +0800

    Hindex

--- src/main/java/shangyin/ShangYin.java
+++ src/main/java/shangyin/ShangYin.java
@@ -6,11 +6,17 @@ package shangyin;
  * @date 2023/8/8 上午9:26
  * <p>
  * <p>
- * 上银开发e云桌面：kf7359  B@s95594!
+ * 上银开发e云桌面：kf7359  B@s95594!  B@s95594!1
  * e连：CS7018   Wkckf7359!
  * 调度平台：admin 123456
  * 邮箱：wangkch@dev.bosc   B@s95594!1
  */
 public class ShangYin {
+    public static void main(String[] args) {
+        int a = 5;
+        a= a>>2;
+        System.out.println(a);
+
+    }
 
 }
