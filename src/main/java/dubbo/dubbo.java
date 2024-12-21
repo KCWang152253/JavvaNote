804c343689c3aa37973732a8ab49b561d086cc17
2747962529@qq.com <2747962529@qq.com>
2024-12-21 22:26:24 +0800

    juc

---
+++ src/main/java/dubbo/dubbo.java
@@ -0,0 +1,43 @@
+package dubbo;
+
+/**
+ * @author KCWang
+ * @version 1.0
+ * @date 2024/12/21 下午10:24
+ */
+public class dubbo {
+    /**
+     *
+     *
+     *
+     *
+     *
+
+
+     启动： dubbo-admin-ui
+         /Users/kcwang/javaWeb/dubbo-admin-develop/dubbo-admin-ui
+         npm run serve  或者   npm run dev
+
+
+
+
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+}
---
+++ src/main/java/zookeeper/zk.java
@@ -0,0 +1,45 @@
+package zookeeper;
+
+/**
+ * @author KCWang
+ * @version 1.0
+ * @date 2024/12/20 下午6:23
+ */
+public class zk {
+    /**
+     *
+     *
+     *
+     *
+
+     启动server:
+         cd   /opt/zookeeper/apache-zookeeper-3.7.2-bin/bin
+         sh zkServer.sh start
+         sh zkServer.sh status  #查看状态
+
+     打开客户端:sh zkCli.sh
+
+
+     //停止
+     $ bin/zkServer.sh stop
+
+
+
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+}
