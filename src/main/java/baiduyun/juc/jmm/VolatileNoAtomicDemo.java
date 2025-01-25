86a2481065ce626428c3790251c1aedd52d5b889
2747962529@qq.com <2747962529@qq.com>
2025-01-25 11:49:22 +0800

    juc

--- src/main/java/baiduyun/juc/jmm/VolatileNoAtomicDemo.java
+++ src/main/java/baiduyun/juc/jmm/VolatileNoAtomicDemo.java
@@ -6,32 +6,32 @@ import java.util.concurrent.TimeUnit;
  * @auther zzyy
  * @create 2021-03-17 14:59
  */
-class MyNumber
-{
-     volatile int number = 0;
+class MyNumber {
+    volatile int number = 0;
 
-    public void addPlusPlus()
-    {
+    public  void  addPlusPlus() {
         number++;
     }
 }
 
-public class VolatileNoAtomicDemo
-{
-    public static void main(String[] args) throws InterruptedException
-    {
+public class VolatileNoAtomicDemo {
+    public static void main(String[] args) throws InterruptedException {
         MyNumber myNumber = new MyNumber();
 
-        for (int i = 1; i <=10; i++) {
+        for (int i = 1; i <= 10; i++) {
             new Thread(() -> {
                 for (int j = 1; j <= 1000; j++) {
                     myNumber.addPlusPlus();
                 }
-            },String.valueOf(i)).start();
+            }, String.valueOf(i)).start();
         }
 
         //暂停几秒钟线程
-        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println(Thread.currentThread().getName() + "\t" + myNumber.number);
     }
 }
