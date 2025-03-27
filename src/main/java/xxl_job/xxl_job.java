72456f89a079a7bd25d5d3c3d010919c4a0ae1b7
U1048811 <eric6.wang@sanofi.com>
2025-03-27 20:01:51 +0800

    a

---
+++ src/main/java/xxl_job/xxl_job.java
@@ -0,0 +1,20 @@
+package xxl_job;
+
+public class xxl_job {
+
+
+    /**
+     *
+     *
+
+     架构网址：  https://www.cnblogs.com/zzyang/p/17876807.html
+
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+}
