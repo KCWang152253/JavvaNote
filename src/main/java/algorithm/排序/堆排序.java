6375546634956a600e1fd551ed8a4d250249fa34
U1048811 <eric6.wang@sanofi.com>
2025-04-01 18:11:46 +0800

    算法

--- src/main/java/algorithm/排序/堆排序.java
+++ src/main/java/algorithm/排序/堆排序.java
@@ -5,6 +5,16 @@ import java.util.Arrays;
 
 public class 堆排序 {
 
+
+    /**
+     * 算法步驟
+     * <p>
+     * 1、将待排序列(R0, R1, ……, Rn)构建成最大堆（最小堆）；
+     * 2、将堆顶元素R[0]与最后一个元素R[n]进行交换，此时得到新的无序区(R0, R1, ……, Rn-1)和新的有序区(Rn),且满足R[0, 1, ……, n-1]<=R[n]（>=R[n]）；
+     * 3、由于调整后的新堆可能违反堆的性质，因此需要对当前无序区(R0, R1, ……, Rn-1)进行调整；
+     * 4、重复步骤2~3直到有序区的元素个数为n。
+     */
+
     private static int heapLen;
 
     public static void main(String[] args) {
@@ -14,72 +24,38 @@ public class 堆排序 {
     }
 
 
+
     public static void heapSort(int[] arr) {
-
         heapLen = arr.length;
-        for (int i = heapLen - 1; i > 0; i--) {
-
-            heapfy(arr, i);
+        for (int i = heapLen - 1; i >= 0; i--) {
+            heapify(arr, i);
         }
 
+        for (int i = heapLen - 1; i > 0; i--) {
+            swap(arr, 0, heapLen - 1);
+            heapLen--;
+            heapify(arr, 0);
+        }
     }
 
-    private static void heapfy(int[] arr, int idx) {
-
-        int left = 2 * idx + 1, right = 2 * idx + 2, largest = idx;
+    private static void heapify(int[] arr, int idx) {
+        int left = idx * 2 + 1, right = idx * 2 + 2, largest = idx;
         if (left < heapLen && arr[left] > arr[largest]) {
             largest = left;
         }
         if (right < heapLen && arr[right] > arr[largest]) {
             largest = right;
         }
+
         if (largest != idx) {
             swap(arr, largest, idx);
-            heapfy(arr, largest);
+            heapify(arr, largest);
         }
-
     }
 
     private static void swap(int[] arr, int idx1, int idx2) {
-
-        int tep = arr[idx1];
+        int tmp = arr[idx1];
         arr[idx1] = arr[idx2];
-        arr[idx2] = tep;
-
-
+        arr[idx2] = tmp;
     }
-
-//    public static void heapSort(int[] arr) {
-//        heapLen = arr.length;
-//        for (int i = heapLen - 1; i >= 0; i--) {
-//            heapify(arr, i);
-//        }
-//
-//        for (int i = heapLen - 1; i > 0; i--) {
-//            swap(arr, 0, heapLen - 1);
-//            heapLen--;
-//            heapify(arr, 0);
-//        }
-//    }
-//
-//    private static void heapify(int[] arr, int idx) {
-//        int left = idx * 2 + 1, right = idx * 2 + 2, largest = idx;
-//        if (left < heapLen && arr[left] > arr[largest]) {
-//            largest = left;
-//        }
-//        if (right < heapLen && arr[right] > arr[largest]) {
-//            largest = right;
-//        }
-//
-//        if (largest != idx) {
-//            swap(arr, largest, idx);
-//            heapify(arr, largest);
-//        }
-//    }
-//
-//    private static void swap(int[] arr, int idx1, int idx2) {
-//        int tmp = arr[idx1];
-//        arr[idx1] = arr[idx2];
-//        arr[idx2] = tmp;
-//    }
 }
