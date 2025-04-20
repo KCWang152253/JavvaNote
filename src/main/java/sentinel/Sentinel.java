63624f874dd5f2daa9a6c0fd8c1bdf48b1d11972
2747962529@qq.com <2747962529@qq.com>
2025-04-20 12:05:02 +0800

    算法

---
+++ src/main/java/sentinel/Sentinel.java
@@ -0,0 +1,27 @@
+package sentinel;
+
+/**
+ * @author KCWang
+ * @version 1.0
+ * @date 2025/4/20 12:03
+ */
+public class Sentinel {
+
+    /**
+     *
+
+
+
+     客户端启动参数
+     -Dcsp.sentinel.dashboard.server=http://localhost:8088 -Dcsp.sentinel.app.name=sentinel-core-example
+
+     问题：404 向控制台注册出现404问题。可能是端口占用有问题 可以换个端口试试
+
+
+     *
+     *
+     *
+     *
+     *
+     */
+}
